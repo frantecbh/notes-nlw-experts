6d0b092f65b335e419d3f444dd1d3b4831641f52
frantecbh <franciscomenezesbh@gmail.com>
2024-02-08 12:27:00 -0300

    layout pagina

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,53 @@
 import logo from './assets/logo-nlw-expert.svg'
 
 export function App() {
-  return <img src={logo} alt="" />
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="Nlw expert" />
+      <form className="w-full">
+        {' '}
+        <input
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+          type="text"
+          placeholder="Busque por suas notas"
+        />
+      </form>
+      <div className="h-px bg-slate-700" />
+      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+        <div className="rounded-md bg-slate-700 p-5 space-y-3">
+          <span className="text-sm font-medium text-slate-200">
+            Adicionar nota
+          </span>
+          <p className="text-sm leading-6 text-slate-400">
+            Grave uma nota em áudio que será convertida para texto
+            automaticamente.
+          </p>
+        </div>
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+          <span className="text-sm font-medium text-slate-300">há 2 dias</span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et,
+            quisquam! Esse, porro quae illum officia praesentium reprehenderit
+            distinctio provident maiores nihil harum accusantium sed sint iure
+            perspiciatis impedit labore consequuntur! Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Et, quisquam! Esse, porro quae illum
+            officia praesentium reprehenderit distinctio provident maiores nihil
+            harum accusantium sed sint iure perspiciatis impedit labore
+            consequuntur!
+          </p>
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+        </div>
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+          <span className="text-sm font-medium text-slate-300">há 4 dias</span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            impedit provident adipisci rem quibusdam distinctio voluptas quod,
+            sunt sed cumque at veniam debitis voluptatem recusandae assumenda
+            natus, vitae hic in?
+          </p>
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+        </div>
+      </div>
+    </div>
+  )
 }
