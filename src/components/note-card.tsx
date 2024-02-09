01239924ea5098efd5791e956232d22ff1d22eec
frantecbh <franciscomenezesbh@gmail.com>
2024-02-09 14:25:13 -0300

    modal apagar

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,5 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import React from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { X } from 'lucide-react'
 
 type NoteCardProps = {
   note: {
@@ -19,9 +21,30 @@ export function NotesCard({ note }: NoteCardProps) {
         <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
       </Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay className="inset-0 fixed bg-black/60" />
-        <Dialog.Content className="z-10 bg-yellow-100 absolute">
-          oi
+        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] h-[60vh] w-full bg-slate-700 rounded-md flex flex-col outline-none">
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <X className="size-5" />
+          </Dialog.Close>
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <span className="text-sm font-medium text-slate-300">
+              {formatDistanceToNow(note.date.toISOString(), {
+                locale: ptBR,
+                addSuffix: true,
+              })}
+            </span>
+            <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+          </div>
+          <button
+            type="button"
+            className="w-full bg-zinc-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
+          >
+            Deseja{' '}
+            <span className="text-red-400 group-hover:underline">
+              apagar esta nota
+            </span>
+            ?
+          </button>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
