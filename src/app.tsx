fa8ed74a4644b5ae321497f50a2525c91bb93389
frantecbh <franciscomenezesbh@gmail.com>
2024-02-09 09:24:57 -0300

    esturutra modal com radix-ui

--- src/app.tsx
+++ src/app.tsx
@@ -2,24 +2,31 @@ import logo from './assets/logo-nlw-expert.svg'
 import { NoteNoteCard } from './components/new-note-card'
 import { NotesCard } from './components/note-card'
 
+const note = {
+  date: new Date(),
+  content: 'Hello world',
+}
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
-      <img src={logo} alt="Nlw expert" />
-      <form className="w-full">
-        {' '}
-        <input
-          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
-          type="text"
-          placeholder="Busque por suas notas"
-        />
-      </form>
-      <div className="h-px bg-slate-700" />
-      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+      <div className="space-y-6 mx-4">
+        <img src={logo} alt="Nlw expert" />
+        <form className="w-full">
+          {' '}
+          <input
+            className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+            type="text"
+            placeholder="Busque por suas notas"
+          />
+        </form>
+        <div className="h-px bg-slate-700" />
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px] gap-6 px-4">
         <NoteNoteCard />
 
-        <NotesCard />
-        <NotesCard />
+        <NotesCard note={note} />
       </div>
     </div>
   )
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,24 +1,29 @@
+import * as Dialog from '@radix-ui/react-dialog'
 import React from 'react'
 
-type NotesCardProps = {
-  title: string
-  note: string
+type NoteCardProps = {
+  note: {
+    date: Date
+    content: string
+  }
 }
 
-export function NotesCard() {
+export function NotesCard({ note }: NoteCardProps) {
   return (
-    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden outline-none relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, quisquam!
-        Esse, porro quae illum officia praesentium reprehenderit distinctio
-        provident maiores nihil harum accusantium sed sint iure perspiciatis
-        impedit labore consequuntur! Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Et, quisquam! Esse, porro quae illum officia
-        praesentium reprehenderit distinctio provident maiores nihil harum
-        accusantium sed sint iure perspiciatis impedit labore consequuntur!
-      </p>
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </button>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md text-left flex flex-col gap-3 bg-slate-800 p-5 overflow-hidden outline-none relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-300">
+          {note.date.toISOString()}
+        </span>
+        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/60" />
+        <Dialog.Content className="z-10 bg-yellow-100 absolute">
+          oi
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
